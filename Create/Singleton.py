5d7728cc1efd2974480469a41abbf32703be9a03
hudi <wangqian1107@163.com>
2022-05-05 15:23:33 +0800

    单例模式【懒汉和单态实现】

--- Create/Singleton.py
+++ Create/Singleton.py
@@ -19,3 +19,44 @@ print(a.a)
 print(b.a)
 print(id(a))
 print(id(b))
+
+#单例模式：懒汉
+class SingleTon:
+    __instance = None
+    def __init__(self):
+        if not SingleTon.__instance:
+            print('__init__ method was called')
+        else:
+            print('Instance has already been created',self.getInstance())
+    @classmethod
+    def getInstance(cls):
+        if not cls.__instance:
+            cls.__instance = SingleTon()
+        return cls.__instance
+print('*************')
+s =  SingleTon()
+print('++++++++++++++')
+print('Object created',SingleTon.getInstance())
+s1 = SingleTon()
+
+
+'''
+单态模式Monostate
+'''
+class Brog:
+    __shared_state = {'1':'hudi'}
+    def __init__(self):
+        self.x = 1
+        self.__dict__ = self.__shared_state
+        pass
+b = Brog()
+b1 = Brog()
+b.x = 45
+b1.x = 85
+b.y = 3
+key = 'hh'
+b.key = 'what'
+print('b',b)
+print('b1',b1)
+print(b.__dict__)
+print(b1.__dict__)
